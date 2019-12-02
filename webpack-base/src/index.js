92691038dea2aca00e4bbc8dbc5a430918f780fd
lifei-2019 <940915122@qq.com>
2019-12-02 10:54:38 +0800

    12-2

--- webpack-base/src/index.js
+++ webpack-base/src/index.js
@@ -7,6 +7,9 @@ import ReactDOM from 'react-dom'    //把创建好的组件和虚拟DOM放到页
 //在JS文件里不能写HTML。但可以用babel来转换JS里的标签
 //JSX是在运行的时候，被转换成了React.creatElement形式来执行的。
 
-const mydiv = <div id="mydiv" title="div aaa">这是一个div</div>
+const mydiv = <div id="mydiv" title="div aaa">
+    这是一个div
+    <h1>这是个h1元素</h1>
+    </div>
 
 ReactDOM.render(mydiv,document.getElementById('app'))
