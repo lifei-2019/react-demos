57488405c7208469e993f9e10d8c6618827478f5
lifei-2019 <940915122@qq.com>
2019-12-09 20:12:21 +0800

    12-09

--- webpack-base/src/index.js
+++ webpack-base/src/index.js
@@ -2,14 +2,23 @@
 import React from 'react'   //创建组件、虚拟DOM元素，生命周期
 import ReactDOM from 'react-dom'    //把创建好的组件和虚拟DOM放到页面上展示
 
+let a = 10
+let str = '你好，中国'
+let boo = true
+let title ='999'
+const h1 = <h1>哈哈哈哈哈哈哈</h1>
 
-
-//在JS文件里不能写HTML。但可以用babel来转换JS里的标签
-//JSX是在运行的时候，被转换成了React.creatElement形式来执行的。
-
-const mydiv = <div id="mydiv" title="div aaa">
-    这是一个div
-    <h1>这是个h1元素</h1>
-    </div>
-
-ReactDOM.render(mydiv,document.getElementById('app'))
+//jsx XML比HTML严格
+//在JSX控制的区域内，写JS表达式，则需要把JS代码写到｛｝里
+ReactDOM.render(<div>
+    {a + 2}
+    <hr/>
+    {str}
+    <hr/>
+    {boo.toString()}
+    {boo ? '条件为真' : '条件为佳'}
+    <hr/>
+    <p title={title}>这是p标签</p>
+    <hr/>
+    {h1}
+    </div>,document.getElementById('app'))
