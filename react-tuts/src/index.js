dd1b2a938a3cf7e4de77f09e926ada24ce194bc4
lifei-2019 <940915122@qq.com>
2020-03-10 23:13:02 +0800

    0310

--- react-tuts/src/index.js
+++ react-tuts/src/index.js
@@ -1,35 +1,29 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { render } from 'react-dom'
 
-//const app=<h1>Welcome React</h1>
+//定义组建的第二种方式，使用类继承React.Component
 
-// const createApp = (props) => {
-//     return (
-//     <div>
-//         {/* 只要在jsx里插入js的代码，就加一层花括号即可，注释也是js，所以这里的注释加了一层花括号 */}
-//         <h1>welcome {props.title}</h1>
-//         <p>优秀的{props.title}</p>
-//     </div>
-//     )
-// }
-
-// const app = createApp({
-//     title: 'React 16.8'
-// })
-
-//创建组建的第一种方式，使用箭头函数，但是这个名字要大写。下面render里面直接写app,就行
-
-
-const App = (props) => {
-    return (
-    <div>
-        {/* 只要在jsx里插入js的代码，就加一层花括号即可，注释也是js，所以这里的注释加了一层花括号 */}
-        <h1>welcome {props.title}</h1>
-        <p>优秀的{props.title}</p>
-    </div>
-    )
+class App extends React.Component{
+    render () {
+        console.log(this.props)
+        return (
+            <div>
+                <h1>类组件继承</h1>
+                <p>{this.props.desc}</p>
+            </div>
+        )
+    }
 }
-ReactDOM.render(
-    <App title="2020" />,
+
+
+//类组件渲染的原理
+// const app = new App({
+//     desc: '类组件是继承React.Component'
+// }).render
+
+//render是react dom提供的方法，通常只会用一次
+render(
+    <App desc="类组件是继承React.Component" />,
     document.querySelector('#root')
 )
+
