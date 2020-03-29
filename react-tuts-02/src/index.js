f21b6c0a4dc5d5ef46a67961dd5368a43bfacba1
lifei-2019 <940915122@qq.com>
2020-03-29 23:07:47 +0800

    0329

--- react-tuts-02/src/index.js
+++ react-tuts-02/src/index.js
@@ -1,14 +1,25 @@
-import React ,{useState} from 'react'
+import React ,{useState, useEffect} from 'react'
 import { render } from 'react-dom'
 
 
 const Counter = () => {
-  console.log(useState())
+  const [count,setCount] = useState(0)
+  //console.log(useState(10))
+  // 可以使用多次
+  // const [title,setTitle] = useState('abc')
+  //console.log(title)
+
+  useEffect(() => {
+    console.log('渲染了')
+    document.title=`当前的数量为${count}`
+  })
+
   return(
     <div>
-      <button>-</button>
-      <span>10</span>
-      <button>+</button>
+      <p>当前的数量为{count}</p>
+      <button onClick={() => {setCount(count - 1)}}>-</button>
+      <span>{count}</span>
+      <button onClick={() => {setCount(count + 1)}}>+</button>
     </div>
   )
 }
