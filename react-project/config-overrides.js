f82b4188285698a0b6c2c586aff53859d0e7b746
lifei-2019 <940915122@qq.com>
2020-04-11 10:14:28 +0800

    基于cra添加react-app-rewired和customize-cra

---
+++ react-project/config-overrides.js
@@ -0,0 +1,11 @@
+/* 
+* @file config-overrides.js
+* @author Rapael Li(Raphael__Li@sina.com)
+* 基于customize和react-app-rewired的定制配置文件
+*/
+
+
+// 从customize-cra引入一些相关的方法
+const { override } = require('customize-cra')
+
+module.exports = override()
