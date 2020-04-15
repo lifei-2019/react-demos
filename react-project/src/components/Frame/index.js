e9068a31bdadc65c65395c4ab15bb116d4e90bbc
lifei-2019 <940915122@qq.com>
2020-04-15 23:54:51 +0800

    0415

--- react-project/src/components/Frame/index.js
+++ react-project/src/components/Frame/index.js
@@ -10,11 +10,6 @@ export default class Frame extends Component {
             <Layout>
             <Header className="header">
               <div className="logo" />
-              <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
-                <Menu.Item key="1">nav 1</Menu.Item>
-                <Menu.Item key="2">nav 2</Menu.Item>
-                <Menu.Item key="3">nav 3</Menu.Item>
-              </Menu>
             </Header>
             <Layout>
               <Sider width={200} className="site-layout-background">
