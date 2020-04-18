f90f2cc3d5ab669e343a87b20ea1f09f4e61b5c4
lifei-2019 <940915122@qq.com>
2020-04-18 23:08:11 +0800

    axios获取数据以及表格的基本渲染

--- react-project/src/views/Article/index.js
+++ react-project/src/views/Article/index.js
@@ -1,68 +1,93 @@
 import React, { Component } from 'react'
-import {Card, Button,Table} from 'antd'
+import moment from 'moment'
+import {Card, Button,Table,Tag} from 'antd'
 
 import { getArticles } from '../../requests'
 
+window.moment=moment
 
-
-
+const titleDisplayMap = {
+  id:'id',
+  title: '标题',
+  author: '作者',
+  createAt: '创作时间',
+  amount: '阅读量'
+}
 
 export default class ArticleList extends Component {
    constructor() {
      super()
      this.state = {
       dataSource: [
-        {
-          key: '1',
-          name: '胡彦斌',
-          age: 32,
-          address: '西湖区湖底公园1号',
-        },
-        {
-          key: '2',
-          name: '胡彦祖',
-          age: 42,
-          address: '西湖区湖底公园1号',
-        },
       ],
       columns: [
-        {
-          title: '姓名',
-          dataIndex: 'name',
-          key: 'name',
-        },
-        {
-          title: '年龄',
-          dataIndex: 'age',
-          key: 'age',
-        },
-        {
-          title: '住址',
-          dataIndex: 'address',
-          key: 'address',
-        },
-        {
-          title: '操作',
-          dataIndex: 'action',
-          key: 'action',
-          render: (text, record,index)=>{
-              // console.log({text,record,index})
-              return <Button>编辑</Button>
-          }
-        },
       ],
       total: 0
      }
    } 
   
-  componentDidMount(){
+
+    createColumns =(columnKeys)=>{
+      return columnKeys.map(item => {
+        if(item === 'amount'){
+          return {
+            title:titleDisplayMap[item],
+            key:item,
+            render: (text,record)=>{
+              // console.log(record)
+              const {amount} =record
+              //这里是根据一个数字的大小做一个条件渲染
+              //同理，可以做发货状态、职位级别不同的颜色
+              //总经理：'001'，经理：'002'，主管'003'
+              // const titleMap={
+              //   '001': 'red',
+              //   '002': '#09f',
+              //   '003':'green'
+              // }
+              // return<Tag color={titleMap{titleKey}}>{record.titleKey}</Tag>
+
+              return <Tag color={amount>200 ? 'red' : 'green'} >{record.amount}</Tag>
+            }
+          }
+        }
+
+
+        if(item === 'createAt'){
+          return {
+            title:titleDisplayMap[item],
+            key:item,
+            render: (text,record)=>{
+              // console.log(record)
+              const {createAt} =record
+
+              return moment(createAt).format('YYYY年MM月DD日 HH:mm:ss秒')
+            }
+          }
+        }
+        return {
+          title:titleDisplayMap[item],
+          dataIndex: item,
+          key:item
+        }
+      })
+    }
+
+    getData =() =>{
       getArticles()
         .then(resp => {
-          console.log(resp)
+          const columnKeys = Object.keys(resp[0].list[0])
+          const columns=this.createColumns(columnKeys)
+          // console.log(columns)
           this.setState({
-            total: resp[0].total
+            total: resp[0].total,
+            dataSource: resp[0].list,
+            columns
           })
         })
+    }
+
+    componentDidMount(){
+      this.getData()
     }
   
   
@@ -72,7 +97,8 @@ export default class ArticleList extends Component {
                 title="文章列表"
                 bordered={false}
                 extra={<Button>导出excel</Button>}  >
-            <Table 
+            <Table
+                rowKey={record=>record.id} 
                 dataSource={this.state.dataSource} 
                 columns={this.state.columns} 
                 // loading={true}
