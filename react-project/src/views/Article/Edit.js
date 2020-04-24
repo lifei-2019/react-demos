a8a6f8508beb07eecb36dc8588bedf86ba14f184
lifei-2019 <940915122@qq.com>
2020-04-24 21:04:06 +0800

    form+wangeditor

--- react-project/src/views/Article/Edit.js
+++ react-project/src/views/Article/Edit.js
@@ -1,16 +1,129 @@
-import React, { Component } from 'react'
+import React, { Component, createRef } from 'react'
 
-import {Card, Button} from 'antd'
-export default class Edit extends Component {
+import {Form, Input, Card, Button, DatePicker} from 'antd'
+import E from 'wangeditor'
+import './edit.less'
+
+const formItemLayout ={
+    labelCol:{
+        span:4
+    },
+    wrapperCol:{
+        span:16
+    }
+}
+
+const tailFormItemLayout = {
+    wrapperCol: {
+      offset:4
+    }
+  };
+
+const onFinish = values => {
+    console.log('Received values of form: ', values);
+};
+
+class Edit extends Component {
+    constructor(){
+        super()
+        this.editorRef = createRef()
+    }
+    formRef = React.createRef();
+    
+    initEditor=()=>{
+        this.editor = new E(this.editorRef.current)
+        this.editor.customConfig.onchange= (html)=>{
+            console.log(this)
+            this.formRef.current.setFieldsValue({
+                content:html
+            })
+        }
+        this.editor.create()
+
+    }
+    componentDidMount(){
+        this.initEditor()
+    }
+
     render() {
         return (
             <Card 
                 title="编辑文章"
                 bordered={false}
-                extra={<Button >导出excel</Button>}  
+                extra={<Button >取消</Button>}  
                 >
-            表单区域
+
+                    <Form
+                        {...formItemLayout}
+                        onFinish={onFinish}
+                        initialValues={{}}
+                        ref={this.formRef}
+                    >
+                    <Form.Item
+                        name="title"
+                        label="标题"
+                        rules={[
+                            { 
+                                required:true,
+                                message:'请填写标题'
+                            }]}
+                    >
+                    <Input placeholder="标题" />
+                    </Form.Item>
+                    <Form.Item
+                        name="author"
+                        label="作者"
+                        rules={[
+                            { 
+                                required:true,
+                                message:'请填写作者'
+                            }]}
+                    >
+                    <Input placeholder="作者" />
+                    </Form.Item>
+                    <Form.Item
+                        name="amount"
+                        label="阅读量"
+                        rules={[
+                            { 
+                                required:true,
+                                message:'请填写阅读量'
+                            }]}
+                    >
+                    <Input placeholder="0" />
+                    </Form.Item>
+                    <Form.Item
+                        name="createAt"
+                        label="编辑时间"
+                        rules={[
+                            { 
+                                required:true,
+                                message:'时间是必选的'
+                            }]}
+                    >
+                    <DatePicker showTime placeholder="选择时间" />
+                    </Form.Item>
+                    <Form.Item
+                        name="content"
+                        label="内容"
+                        rules={[
+                            { 
+                                required:true,
+                                message:'内容是必写的'
+                            }]}
+                    >
+                    <div className="qf-editor" ref={this.editorRef} />
+                    </Form.Item>
+                    <Form.Item {...tailFormItemLayout}>
+                        <Button type="primary" htmlType="submit">
+                        保存
+                        </Button>
+                    </Form.Item>
+                    
+                    </Form>
             </Card>
         )
     }
 }
+
+export default Edit
