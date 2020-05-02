462c78a7e839efce02b1bbe245e89d51546e8b47
lifei-2019 <940915122@qq.com>
2020-05-02 22:00:21 +0800

    登录界面UI界面

--- react-project/src/views/Login/index.js
+++ react-project/src/views/Login/index.js
@@ -1,11 +1,65 @@
 import React, { Component } from 'react'
+import { Form, Input, Button, Checkbox, Card } from 'antd'
+import { UserOutlined, LockOutlined } from '@ant-design/icons'
+import { withRouter } from 'react-router-dom'
 
-export default class Login extends Component {
+import './login.less'
+
+@withRouter
+class Login extends Component {
+
+    onFinish = values => {
+        console.log('Received values of form: ', values);
+    }
+
     render() {
         return (
-            <div>
-                登录
-            </div>
-        )
+          <Card
+            title="uricorn系统登录"
+            className="uricorn-login-wrapper">
+            <Form
+              name="normal_login"
+              className="login-form"
+              initialValues={{ remember: true }}
+              onFinish={this.onFinish}
+            >
+              <Form.Item
+                name="username"
+                rules={[{ required: true, message: '请输入用户名！' }]}
+              >
+                <Input 
+                  prefix={<UserOutlined className="site-form-item-icon" />} 
+                  placeholder="用户名" />
+              </Form.Item>
+              <Form.Item
+                name="password"
+                rules={[{ required: true, message: '请输入密码!' }]}
+              >
+                <Input
+                  prefix={<LockOutlined className="site-form-item-icon" />}
+                  type="password"
+                  placeholder="密码"
+                />
+              </Form.Item>
+              <Form.Item>
+                <Form.Item name="remember" valuePropName="checked" noStyle>
+                  <Checkbox>记住我</Checkbox>
+                </Form.Item>
+        
+                <a className="login-form-forgot" href="">
+                  Forgot password
+                </a>
+              </Form.Item>
+        
+              <Form.Item>
+                <Button type="primary" htmlType="submit" className="login-form-button">
+                  登录
+                </Button>
+                Or <a href="">register now!</a>
+              </Form.Item>
+            </Form>
+          </Card>
+          )
     }
 }
+export default Login
